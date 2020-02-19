5c1dbce604a81068a79fa41c1f947d630786ef21
Noppanat1997 <architon1997@gmail.com>
2020-02-20 00:31:58 +0700

    first commit

---
+++ src/reducer.js
@@ -0,0 +1,34 @@
+const initialState = {
+  curPage: 0,
+  data: [
+    { id: 0, line: [] },
+    { id: 1, line: [] },
+    { id: 2, line: [] },
+    { id: 3, line: [] },
+    { id: 4, line: [] }
+  ]
+}
+const reducer = (state = initialState, action) => {
+  const newState = {...state};
+  switch (action.type) {
+    case 'CHANGE_PAGE':
+      const changedPage = { curPage: action.payload.curPage }
+      newState.curPage = changedPage.curPage;
+      if (action.payload.curPage > state.data.length - 1) {
+        newState.data.push({ id: action.payload.curPage, line: [] })
+      }
+
+      console.log(newState)
+      return newState
+
+    case 'ADD_LINE':
+      const { id, line: newLine } = action.payload;
+      newState.data[id].line.push(newLine)
+
+      return newState;
+    default:
+      break;
+  }
+  return state;
+}
+export default reducer;
