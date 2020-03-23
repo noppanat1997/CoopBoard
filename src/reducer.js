7b8eef256cdc96dd569985d80f2daa237e0314dc
SaltAtWork <60347847+SaltAtWork@users.noreply.github.com>
2020-03-23 14:44:12 +0700

    Eraser and Redraw 23/3

--- src/reducer.js
+++ src/reducer.js
@@ -5,7 +5,14 @@ const initialState = {
     { id: 2, line: [] },
     { id: 3, line: [] },
     { id: 4, line: [] }
-  ]
+  ],
+  buttonData: {
+    1:{ isActive: 0},
+    2:{ isActive: 0},
+    3:{ isActive: 1},
+    4:{ isActive: 0},
+    5:{ isActive: 0}
+  }
 }
 const reducer = (state = initialState, action) => {
   const newState = {...state};
@@ -20,11 +27,38 @@ const reducer = (state = initialState, action) => {
       console.log(newState)
       return newState
 
-    case 'ADD_LINE':
-      const { id, line: newLine } = action.payload;
-      newState.data[id].line.push(newLine)
-
+    //case 'ADD_LINE':
+    //  const { id, line: newLine } = action.payload;
+    //  newState.data[id].line.push(newLine)
+    //  return newState;
+      
+    case 'UPDATE_LINE':
+      const {id, data: updatedLineData , mode} = action.payload;
+      if(mode == 1){
+        newState.data[id].line.push(updatedLineData)
+      }
+      else if(mode == 2){
+        for(var i = updatedLineData.length; i > 0;i--){
+          const t = updatedLineData.pop();
+          newState.data[id].line.splice(t,1);
+        }
+      }
       return newState;
+
+    case 'TOGGLE_BUTTON':
+      const { newId, newIsActive } = action.payload;
+      const newbuttonData = {
+        1:{ isActive: 0},
+        2:{ isActive: 0},
+        3:{ isActive: 0},
+        4:{ isActive: 0},
+        5:{ isActive: 0}
+      }
+      newbuttonData[newId].isActive = newIsActive
+      return {
+        ...state,
+        buttonData: newbuttonData
+      }
     default:
       break;
   }
