ac976c42584aa93e859ffa41494dac9942c0d861
Nitikorn <ichromex@hotmail.com>
2020-04-04 09:46:33 +0700

    registerV2

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -8,15 +8,12 @@ const RegisterPages = () => {
 
   return (
     <div className="bg">
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-3"></div>
-          <Card style={{ width: '536px', height: '536px' }}>
+          <Card className="register-card" style={{ width: '536px', height: '536px' , color:'#C1C1C1' }}>
             <Card.Body>
               <Form>
                 <h1 style={{color:'#D4145A'}}>SIGN UP</h1>
                 <h5 style={{color:'#D4145A'}}>Please fill in this form to create an account!</h5>
-                <Form.Row>
+                <Form.Row className="mb-2">
                   <Col>
                     <Form.Control placeholder="Fisrt name" />
                   </Col>
@@ -24,11 +21,11 @@ const RegisterPages = () => {
                     <Form.Control placeholder="Last name" />
                   </Col>
                 </Form.Row>
-                <Form.Group>
+                <Form.Group className="mb-2">
                   <Form.Control type="email" placeholder="Email" />
                 </Form.Group>
                 <Form.Group>
-                  <Form.Control type="password" placeholder="Password" />
+                  <Form.Control className="mb-2" type="password" placeholder="Password" />
                   <Form.Control type="password" placeholder="Confirm password" />
                 </Form.Group>
                 <div className="text-center">
@@ -39,8 +36,6 @@ const RegisterPages = () => {
               </Form>
             </Card.Body>
           </Card>
-        </div>
-      </div>
 
       {/* <Card>
         <Card.Body>Hello</Card.Body>
