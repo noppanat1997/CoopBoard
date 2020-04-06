b43e66caeba82387b082961ca9070bfe9498da82
Noppanat1997 <architon1997@gmail.com>
2020-04-06 20:06:50 +0700

    non

--- src/components/CardField.js
+++ src/components/CardField.js
@@ -3,18 +3,37 @@ import '.././css/CarouselComponent.css';
 import Draggable from 'react-draggable';
 import EachCard from './EachCard.js';
 import '.././css/CardField.css';
-const CardField = (props) => {
+import { connect } from 'react-redux';
 
+const CardField = (props) => {
+  const cardDataList = props.stateFromStore.cardData[props.page].data
+  let lists
+  if (cardDataList.length !== 0) {
+    lists = cardDataList.map(item =>
+      (
+        <Draggable key={item.id}>
+          <div>
+            <EachCard id="1" name={item.text} color={"#D4145A"} />
+          </div>
+        </Draggable>
+      ))
+  }
   return (
     <div>
-      <Draggable>
-        <div>
-          <EachCard id="1" name={"Post-It"} color={"#D4145A"} />
-        </div>
-      </Draggable>
+      {lists}
     </div>
-    
   );
 }
-
-export default CardField;
+const mapStateToProps = state => {
+  return {
+    stateFromStore: state
+  }
+}
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     updateLine: (updateLine) => {
+//       return dispatch({ type: 'UPDATE_LINE', payload: updateLine });
+//     }
+//   }
+// }
+export default connect(mapStateToProps, null)(CardField);
