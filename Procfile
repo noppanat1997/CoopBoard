d476957cd58d37f8404f9a0c099e5cffcfbab2b9
Noppanat1997 <architon1997@gmail.com>
2020-05-01 18:04:08 +0700

    tried

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: webpack --mode production --config ./src/webpack.config.js && babel-node --optimize_for_size --max_old_space_size=460 --ignore ./src ./server/index.js
--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(routeHandler);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
    console.log(`Server is running on PORT ${port}`);
