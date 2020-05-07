9fa5860698bbf83a7ea22fd584d89d2eb760932d
J4deSPK <jade.supakorn@hotmail.com>
2020-05-07 10:22:02 +0700

    Change name chat to UPPERCASE

--- src/components/chat/Message.js
+++ src/components/chat/Message.js
@@ -7,7 +7,7 @@ import ReactEmoji from 'react-emoji';
 const Message = ({ message: { text, user }, name }) => {
   let isSentByCurrentUser = false;
 
-  const trimmedName = name.trim().toLowerCase();
+  const trimmedName = name.trim().toUpperCase();
 
   if(user === trimmedName) {
     isSentByCurrentUser = true;
